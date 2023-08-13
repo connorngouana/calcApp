1c8ef606ab34c60a7843e32eb50db041e8c8e7b3
connorngouana <connorngouana@gmail.com>
2023-08-13 16:07:05 +0100

    created new branch called feature_sub with code

--- src/test/java/ie/atu/week5/calculatorTest.java
+++ src/test/java/ie/atu/week5/calculatorTest.java
@@ -2,21 +2,27 @@ package ie.atu.week5;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class calculatorTest
 {
-
+    calculator myResult;
     @BeforeEach
     void setUp()
     {
+        myResult = new calculator();
+    }
 
+    @Test
+    void testSubtract()
+    {
+        assertEquals(4, myResult.subtract(5,1));
     }
 
     @AfterEach
     void tearDown()
     {
-
     }
 }
