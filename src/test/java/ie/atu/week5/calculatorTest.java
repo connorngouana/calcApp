f3bcd298c7ea93b6514ee7cb6780ef054ce0d1ae
connorngouana <connorngouana@gmail.com>
2023-08-13 15:49:27 +0100

    Created new classes for calculator

---
+++ src/test/java/ie/atu/week5/calculatorTest.java
@@ -0,0 +1,22 @@
+package ie.atu.week5;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class calculatorTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+
+    }
+}
