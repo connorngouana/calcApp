b8c63561cb0b8e6ddde0d0767286a3f652e10501
connorngouana <connorngouana@gmail.com>
2023-08-13 16:02:10 +0100

    Created new classes for calculator and calcultor test with code

--- src/test/java/ie/atu/week5/calculatorTest.java
+++ src/test/java/ie/atu/week5/calculatorTest.java
@@ -2,16 +2,22 @@ package ie.atu.week5;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class calculatorTest
 {
-
+    calculator myResult;
     @BeforeEach
     void setUp()
     {
-
+        myResult = new calculator();
+    }
+    @Test
+    void testadd()
+    {
+        assertEquals(10, myResult.add(5,5));
     }
 
     @AfterEach
