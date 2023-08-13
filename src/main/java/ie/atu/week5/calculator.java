c47247183ce34c22780b53167a47dae4e461c07e
connorngouana <connorngouana@gmail.com>
2023-08-13 16:11:22 +0100

    created new branch called feature_multi with code

--- src/main/java/ie/atu/week5/calculator.java
+++ src/main/java/ie/atu/week5/calculator.java
@@ -1,5 +1,25 @@
 package ie.atu.week5;
 
-public class calculator {
+public class calculator
+{
+    private int val1;
+    private int val2;
+    private int result;
+
+    public calculator()
+    {
+        val1 = 0;
+        val2 = 0;
+        result = 0;
+    }
+    public int multiply(int val1, int val2)
+    {
+        result = val1 * val2;
+        return result;
+    }
+
+
+
+
 
 }
--- src/test/java/ie/atu/week5/calculatorTest.java
+++ src/test/java/ie/atu/week5/calculatorTest.java
@@ -2,16 +2,23 @@ package ie.atu.week5;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class calculatorTest
 {
-
+    calculator myResult;
     @BeforeEach
     void setUp()
     {
+        myResult = new calculator();
+    }
 
+    @Test
+    void testMultiply()
+    {
+        assertEquals(20, myResult.multiply(10,2));
     }
 
     @AfterEach
